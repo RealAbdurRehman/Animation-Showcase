e8c3be42d418fde417146737ab5a297bb2d8886f
Abdur Rehman <claudethebitplayer@gmail.com>
2025-02-15 12:25:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,70 @@
+import InputHandler from "./InputHandler.js";
+
 window.addEventListener("load", function () {
   const loadingScreen = document.getElementById("loading-screen");
   const loadingContent = document.querySelector(".loading-content");
-  
-  loadingContent.innerHTML = "<h1>Loaded!</h1>";
-  setTimeout(() => { loadingScreen.style.opacity = 0 }, 750);
 
+  loadingContent.innerHTML = "<h1>Loaded!</h1>";
+  setTimeout(() => {
+    loadingScreen.style.opacity = 0;
+  }, 750);
 
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   const CANVAS_WIDTH = (canvas.width = window.innerWidth);
   const CANVAS_HEIGHT = (canvas.height = window.innerHeight);
 
-  let playerImage = document.getElementById("idle1");
-  const player = { width: 799 * 0.5, height: 736 * 0.5, isMoving: false, isPunching: false, facingLeft: false };
+  const player = createPlayer();
+  const inputHandler = new InputHandler();
+
   let imageToShow = 0;
   let animationToShow = 0;
   let gameFrame = 0;
-  const startY = CANVAS_HEIGHT - player.height;
-  let startX = 0;
   const frameStagger = 2;
   const spriteAnimations = [
     {
       name: "idle",
-      frame: Array.from(
-        { length: 20 },
-        (_, i) => document.getElementById(`idle${i + 1}`)
+      frame: Array.from({ length: 20 }, (_, i) =>
+        document.getElementById(`idle${i + 1}`)
       ),
     },
     {
       name: "punching",
-      frame: Array.from(
-        { length: 10 },
-        (_, i) => document.getElementById(`punching${i + 1}`)
+      frame: Array.from({ length: 10 }, (_, i) =>
+        document.getElementById(`punching${i + 1}`)
       ),
     },
     {
       name: "running",
-      frame: Array.from(
-        { length: 25 },
-        (_, i) => document.getElementById(`running${i + 1}`)
+      frame: Array.from({ length: 25 }, (_, i) =>
+        document.getElementById(`running${i + 1}`)
       ),
-    }
+    },
   ];
 
-  function moveRight() {
-    if (startX < CANVAS_WIDTH - player.width) {
-      startX += 15;
+  function createPlayer() {
+    const height = 736 * 0.5;
+    return {
+      width: 799 * 0.5,
+      height: height,
+      isMoving: false,
+      isPunching: false,
+      facingLeft: false,
+      image: document.getElementById("idle1"),
+      position: { x: 0, y: CANVAS_HEIGHT - height },
+    };
+  }
+
+  function movePlayer() {
+    const keys = inputHandler.keys;
+
+    if (keys.includes("KeyA") || keys.includes("ArrowLeft")) {
+      player.position.x -= player.isPunching ? 0.5 : 7;
+      animationToShow = 2;
+      player.isMoving = true;
+      player.facingLeft = true;
+    } else if (keys.includes("KeyD") || keys.includes("ArrowRight")) {
+      player.position.x += player.isPunching ? 0.5 : 7;
       animationToShow = 2;
       player.isMoving = true;
       player.facingLeft = false;
@@ -53,100 +72,61 @@ window.addEventListener("load", function () {
       animationToShow = 0;
       player.isMoving = false;
     }
-  }
 
-  function moveLeft() {
-    if (startX) {
-      startX -= 15;
-      animationToShow = 2;
-      player.isMoving = true;
-      player.facingLeft = true;
-    } else {
-      animationToShow = 0;
-      player.isMoving = false;
-    }
-  }
-
-  function startPunching() {
-    if (!player.isPunching) {
+    if (keys.includes("KeyF")) {
       animationToShow = 1;
       player.isPunching = true;
-      player.isMoving = false;
-    }
+    } else player.isPunching = false;
+
+    handleBoundaries();
   }
 
-  function stopPunching() {
-    if (player.isPunching) {
-      player.isPunching = false;
-      if (player.isMoving) {
-        animationToShow = 2;
-      } else {
-        animationToShow = 0;
-      }
+  function handleBoundaries() {
+    if (player.position.x <=  -player.width * 0.35) {
+      player.position.x = -player.width * 0.35;
+      animationToShow = player.isPunching ? 1 : 0;
+    } else if (player.position.x >= CANVAS_WIDTH - player.width * 0.65) {
+      player.position.x = CANVAS_WIDTH - player.width * 0.65;
+      animationToShow = player.isPunching ? 1 : 0;
     }
   }
-
-  window.addEventListener("keydown", event => {
-    switch (event.key) {
-      case "ArrowRight":
-      case "D":
-      case "d":
-        moveRight();
-        break;
-      case "ArrowLeft":
-      case "A":
-      case "a":
-        moveLeft();
-        break;
-      case "F":
-      case "f":
-        startPunching();
-        return;
-    }
-  });
-
-  window.addEventListener("keyup", () => {
-    if (player.isMoving) {
-      animationToShow = 0;
-    } else {
-      stopPunching();
-    }
-  })
-
-  window.addEventListener("mousedown", event => {
-    switch (event.button) {
-      case 0:
-        startPunching();
-        break;
-    }
-  })
-
-  window.addEventListener("mouseup", () => {
-    stopPunching();
-  })
 
   function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.save();
     if (player.facingLeft) {
       ctx.scale(-1, 1);
-      ctx.drawImage(playerImage, -startX - player.width, startY, player.width, player.height);
+      ctx.drawImage(
+        player.image,
+        -player.position.x - player.width,
+        player.position.y,
+        player.width,
+        player.height
+      );
     } else {
-      ctx.drawImage(playerImage, startX, startY, player.width, player.height);
+      ctx.drawImage(
+        player.image,
+        player.position.x,
+        player.position.y,
+        player.width,
+        player.height
+      );
     }
 
     ctx.restore();
 
+    movePlayer();
+
     const currentAnimation = spriteAnimations[animationToShow].frame;
     if (gameFrame % frameStagger === 0) {
       if (imageToShow < currentAnimation.length) {
-        playerImage = currentAnimation[imageToShow];
+        player.image = currentAnimation[imageToShow];
         imageToShow++;
       } else {
         imageToShow = 0;
       }
     }
-    
+
     gameFrame++;
     requestAnimationFrame(animate);
   }
